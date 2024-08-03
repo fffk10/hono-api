8e5f2e7ffcd942241630dc12105b3943cbd2e5b4
fffk10 <keiju.fujiwara@ncdc.co.jp>
2024-08-04 03:31:06 +0900

    static file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import { Hono } from "hono";
+import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
+// static file serving
+app.use("/static/*", serveStatic({ root: "./" }));
+app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
+app.get("/", (c) => c.text("You can access: /static/hello.txt"));
+app.get("*", serveStatic({ path: "./static/fallback.txt" }));
+
+app.get(
+  "/static/*",
+  serveStatic({
+    onNotFound: (path, c) => {
+      console.log(`${path} is not found, you access ${c.req.path}`);
+    },
+  })
+);
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
@@ -10,4 +25,7 @@ app.get("/entry/:date/:id", (c) => {
   return c.text(`Entry ${id}`);
 });
 
-export default app;
+export default {
+  port: 3333,
+  fetch: app.fetch,
+};
