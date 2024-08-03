d4cb71e38965dba1fdfcbd3a8d01e587f9f96ce0
fffk10 <keiju.fujiwara@ncdc.co.jp>
2024-08-04 04:08:51 +0900

    logger

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/bun";
+import { logger } from "hono/logger";
 
 const app = new Hono();
+
+// logger
+app.use(logger());
 
 // static file serving
 app.use("/static/*", serveStatic({ root: "./" }));
 app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 app.get("/", (c) => c.text("You can access: /static/hello.txt"));
-app.get("*", serveStatic({ path: "./static/fallback.txt" }));
-
 app.get(
   "/static/*",
   serveStatic({
@@ -17,12 +19,28 @@ app.get(
     },
   })
 );
+
+// routes
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 app.get("/entry/:date/:id", (c) => {
   const id = c.req.param("id");
   return c.text(`Entry ${id}`);
+});
+
+app.get("/check/error", (c) => {
+  throw new Error("request error route");
+});
+
+// custom
+app.notFound((c) => {
+  return c.text("404 Not Found");
+});
+
+app.onError((err, c) => {
+  console.error(`${err}`);
+  return c.text("Custom Error Message", 500);
 });
 
 export default {
